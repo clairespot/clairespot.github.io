c7bee70dd3cbdac5602e24b4180d10b7bdce8a41
Filippo Ragazzo <filipporagazzo89@gmail.com>
2023-11-29 22:34:14 +0100

    Use Formspree with reCAPTCHA

--- margheranelmezzo/js/form.js
+++ margheranelmezzo/js/form.js
@@ -36,3 +36,26 @@ function onSubmitForm() {
 function goToIndex() {
     window.location.href = window.location.origin + '/margheranelmezzo';
 }
+
+function manageLoad() {
+    if (window.history.replaceState) {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const load = urlParams.get('load');
+
+        if (load === null) {
+            window.history.replaceState({}, 'Marghera nel mezzo', window.location.href + '?load=true');
+        } else {
+            window.location.href = window.location.href.replace('form.html?load=true', '')
+        }
+    }
+}
+
+// Make reCAPTCHA required
+window.onload = function() {
+    var el = document.getElementById('g-recaptcha-response');
+    if (el) {
+        el.setAttribute('required', 'required'); 
+    }
+    manageLoad();
+}
