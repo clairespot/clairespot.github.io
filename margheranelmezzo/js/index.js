016c4abb2570a3a3dc2bfc3c0cd35e3166ec5e6c
Filippo Ragazzo <filipporagazzo89@gmail.com>
2023-12-03 14:38:05 +0100

    Add contents

--- margheranelmezzo/js/index.js
+++ margheranelmezzo/js/index.js
@@ -1,28 +1,15 @@
 const contentData = [
-    // {
-    //     id: 'picture',
-    //     content: '<div class="grid-item"><img src="https://placehold.co/300x200" alt="Sample Picture"></div><p>Sample Picture Description</p>'
-    // },
-    // {
-    //     id: 'video',
-    //     content: '<iframe width="300" height="200" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe><p>Sample Video Description</p>'
-    // },
-    // {
-    //     id: 'text',
-    //     content: '<p>Sample Full Text Content: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempus mi aliquam risus malesuada consequat. Suspendisse vehicula tempor diam, sit amet consequat est. Cras at lacinia urna. Nam at velit sem. Mauris nec odio aliquet nulla blandit imperdiet. Maecenas ornare erat id felis ornare congue. Morbi fringilla efficitur libero quis lobortis. Pellentesque rhoncus elit at ipsum aliquet varius. Maecenas pretium imperdiet magna eu posuere. Vivamus massa magna, sagittis sit amet sodales sit amet, interdum et eros. Cras ut pretium nisi, a imperdiet velit. Aenean pellentesque sollicitudin lacus vestibulum ultricies. Nulla euismod id purus vel interdum. Aliquam a iaculis ipsum. Sed iaculis, magna a venenatis viverra, risus felis laoreet augue, in egestas ligula augue id libero.</p>'
-    // }
-
     {
         id: 'the-bridge',
         content: `
             <video width="320" height="240" controls>
                 <source src="files/the-bridge-30.mp4" type="video/mp4">
-                <source src="files/the-bridge.ogg" type="video/ogg">
+                <source src="files/the-bridge-30.ogg" type="video/ogg">
                 Video non supportato.
             </video>
-            <p align="left">Autore: John Doe</p>
-            <p align="left">Anno: 2023</p>
-            <p align="left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempus mi aliquam risus malesuada consequat. Suspendisse vehicula tempor diam, sit amet consequat est.</p>
+            <p align="left">Author: The Bridge (Lorenzo Lazzari, Luca Boscolo, Marta Grasp, Giova Rossi, Giulio Rossato, Riccardo Vendramin)</p>
+            <p align="left">Yeat: 2022</p>
+            <p align="left">Description: The Bridge performing at “Concerto contro la guerra” (Concert against the war). WAR IS NEVER THE SOLUTION.</p>
         `
     },
     {
@@ -43,9 +30,122 @@ const contentData = [
                 <div class="grid-item"><img src="files/vicky/tinified/11.jpg" alt="Sample Picture"></div>
                 <div class="grid-item"><img src="files/vicky/tinified/12.jpg" alt="Sample Picture"></div>
             </div>
-            <p align="left">Autore: John Doe</p>
-            <p align="left">Anno: 2023</p>
-            <p align="left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempus mi aliquam risus malesuada consequat. Suspendisse vehicula tempor diam, sit amet consequat est.</p>
+            <p align="left">Author: La Vicky</p>
+            <p align="left">Year: 2023</p>
+            <p align="left">Description: Marghera at night-time, captured during long explorative walks.</p>
+        `
+    },
+    {
+        id: 'vale',
+        content: `
+            <p>
+                I grew up<br/>
+                Calling home<br/>
+                Violence and drugs<br/>
+                Needles and whores<br/>
+                A grey place<br/>
+                Where dreams<br/>
+                Would hardly grow<br/>
+                <br/>
+                Then why,<br/>
+                For fuck's sake,<br/>
+                My eyes prickle<br/>
+                For blinking lights,<br/>
+                Tall trees and fireflies?<br/>
+                Echoing laughter<br/>
+                And Tears and fears<br/>
+                Choking on nostalgia<br/>
+                <br/>
+                Shining jewels<br/>
+                In the form of people<br/>
+                Is all I wanna keep<br/>
+                Of what I once<br/>
+                Called home
+            </p>
+            <p align="left">Author: Valentina Bolani</p>
+            <p align="left">Year: 2023</p>
+        `
+    },
+    {
+        id: 'bro',
+        content: `
+            <video width="320" height="240" controls>
+                <source src="files/bro.mp4" type="video/mp4">
+                <source src="files/bro.ogg" type="video/ogg">
+                Video non supportato.
+            </video>
+            <p align="left">Author: Bruno Mameli</p>
+            <p align="left">Yeat: 2022</p>
+            <p align="left">Description: Aerial view of Marghera.</p>
+        `
+    },
+    {
+        id: 'elisabetta',
+        content: `
+            <div class="img-container grid">
+                <div class="grid-sizer"></div>
+                <div class="grid-item"><img src="files/elisabetta/1.jpeg" alt="Sample Picture"></div>
+                <div class="grid-item"><img src="files/elisabetta/2.jpeg" alt="Sample Picture"></div>
+                <div class="grid-item"><img src="files/elisabetta/3.jpeg" alt="Sample Picture"></div>
+            </div>
+            <p align="left">Author: Elisabetta Castellano</p>
+            <p align="left">Year: Various</p>
+            <p align="left">Description: Glimpses of Marghera.</p>
+        `
+    },
+    {
+        id: 'bro2',
+        content: `
+            <div class="single-image">
+                <img src="files/bro2/1.jpg" alt="Sample Picture"></div>
+            </div>
+            <p align="left">Author: Bruno Mameli</p>
+            <p align="left">Year: 2022</p>
+            <p align="left">Description: Glimpse of a residential area of Marghera, near via Fratelli Bandiera.</p>
+        `
+    },
+    {
+        id: 'michele',
+        content: `
+            <div class="single-image">
+                <img src="files/michele/1.png" alt="Sample Picture"></div>
+            </div>
+            <p align="left">Author: Michele Cor&#242;</p>
+            <p align="left">Year: 1978 or 1980</p>
+            <p align="left">Description: Glimpse of Marghera, showing San Michele church framed by some residential buildings.</p>
+        `
+    },
+    {
+        id: 'paola',
+        content: `
+            <div class="single-image">
+                <img src="files/paola/1.jpg" alt="Sample Picture"></div>
+            </div>
+            <p align="left">Author: Paola Cor&#242;</p>
+            <p align="left">Year: 2023</p>
+            <p align="left">Description: Photo of the vegetation of Piazzale Martiri delle Foibe, typical roundabout in Marghera.</p>
+        `
+    },
+    {
+        id: 'lucia',
+        content: `
+            <div class="single-image">
+                <img src="files/lucia/1.jpg" alt="Sample Picture"></div>
+            </div>
+            <p align="left">Author: Lucia Portinari</p>
+            <p align="left">Year: 2023</p>
+            <p align="left">Description: Via Frateli Bandiera in the sunlight.</p>
+        `
+    },
+    {
+        id: 'denis',
+        content: `
+            <div class="single-image">
+                <img src="files/denis/1.jpg" alt="Sample Picture"></div>
+            </div>
+            <p align="left">Author: Denis Ughelini</p>
+            <p align="left">Year: 2023</p>
+            <p align="left">Description: 30175% FROM MARGHERA.</p>
         `
     }
 ];
@@ -61,8 +161,6 @@ function displayRandomContent() {
         randomIndex = Math.floor(Math.random() * contentData.length);
     } while (contentData[randomIndex].id === currentContentId);
 
-    // randomIndex = 1; // TESTING
-
     // Update the current content id
     currentContentId = contentData[randomIndex].id;
 
@@ -71,7 +169,7 @@ function displayRandomContent() {
     document.getElementById('content').innerHTML = randomContent.content;
 
     // If they're images, apply Masonry
-    if (currentContentId == 'vicky') {
+    if (currentContentId == 'vicky' || currentContentId == 'elisabetta') {
         var grid = document.querySelector('.grid');
         var msnry;
         imagesLoaded(grid, function() {
